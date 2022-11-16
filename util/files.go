2ccdaa79dfab9ab117738ba47dba3d8a886aede4
Aurelio Buonomo <auribuo@gmail.com>
2022-11-16 10:10:10 +0100

    Cross platform paths in upload

--- util/files.go
+++ util/files.go
@@ -2,11 +2,12 @@ package util
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/viper"
 )
 
 func FileName(name string, ext string) string {
 	basePath := viper.GetString("storage.content_root")
-	return fmt.Sprintf("%s/%s.%s", basePath, name, ext)
+	return fmt.Sprintf("%s%c%s.%s", basePath, os.PathSeparator, name, ext)
 }
